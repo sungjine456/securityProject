cc6baf656c2ae153a9cede7aef232561d6ed6454
sungjine456 <sungjine456@gmail.com>
2016-11-03 20:43:57 +0900

    DB 연동

--- src/main/java/com/security/config/SecurityConfig.java
+++ src/main/java/com/security/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.security.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired DataSource dataSource;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -17,13 +27,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin().loginPage("/login").successForwardUrl("/admin").permitAll()
 			.and()
 			.logout().logoutSuccessUrl("/");
-	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("admin")
-			.password("1234")
-			.authorities("ROLE_ADMIN");
 	}
 }
